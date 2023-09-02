64166ce23f3d8ff74054330853095509bb296ab4
Pedrowill21 <pedrowilliam2999@gmail.com>
2023-09-02 00:40:33 -0300

    'game'

--- quiz/src/components/Form.jsx
+++ quiz/src/components/Form.jsx
@@ -1,14 +1,28 @@
 import styles from "./Form.module.css"
-import React from 'react'
+import React, { useState } from 'react'
 
 const Form = ({lista}) => {
-    const {o}
+    const [nome1, setNome1] = useState(lista[0].name)
+    const [nome2, setNome2] = useState(lista[1].name)
+    const [nome3, setNome3] = useState(lista[2].name)
+    const [opcao,setEscolha] = useState("")
+
+    function escolha(opcaoEscolhida){
+        setEscolha(opcaoEscolhida)
+    }
+  
+    function handleSubmit(){
+
+    }
 
 
   return (
     <div>
-        <form>
-            <input type="text" value={}  disabled/>
+        <form className={styles.form}>
+            <div className={opcao === nome1 ? "inputEscolhido" : "input"} onClick={()=>escolha(nome1)}>{nome1}</div>
+            <div className={opcao === nome2 ? "inputEscolhido" : "input"} onClick={()=>escolha(nome2)}>{nome2}</div>
+            <div className={opcao === nome3 ? "inputEscolhido" : "input"} onClick={()=>escolha(nome3)}>{nome3}</div>
+            
         </form>
     </div>
   )
